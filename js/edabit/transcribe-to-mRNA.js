2241646b602ea3e3974f218f904d6cfe36ec30bf
diabm1 <diabm1855@gmail.com>
2022-11-08 22:53:15 -0500

    code formatter

--- js/edabit/transcribe-to-mRNA.js
+++ js/edabit/transcribe-to-mRNA.js
@@ -35,23 +35,30 @@ dna.replaceAll("C", "G"); //replaces all occurences of "C" with "G" and returns 
 
 // console.log(dna)
 //   console.log(dna.split("")[0]);
-let dnaSplitArr = dna.split("");//string array
-let mRnaSplitArr = mRna.split("");//string array
+let dnaSplitArr = dna.split(""); //string array
+let mRnaSplitArr = mRna.split(""); //string array
 // console.log(typeof dnaSplitArr.join("")) //== string
 
 //what if I just replace each letter
 // console.log(typeof dnaSplitArr[0]) //string
 //because .replaceAll() only works on string from my knowledge so far
 
-for (let i = 0; i < array.length; i++) {
-  dnaSplitArr[0].replaceAll("A", "U") + " ---> " + dnaSplitArr[0]
-  
+// I need to cycle through in order to get a
+for (let i = 0; i < dnaSplitArr.length; i++) {
+  for (let j = 0; j < mRnaSplitArr.length; j++) {
+    // console.log(dnaSplitArr[i].replaceAll(`${dnaSplitArr[i]}`, `${mRnaSplitArr[j]}`) +
+    //   " ---> " +
+    //   dnaSplitArr[i]);
+    console.log(
+      dnaSplitArr[i].replaceAll(`${dnaSplitArr[i]}`, `${mRnaSplitArr[j]}`)
+    );
+  }
 }
 
-console.log(dnaSplitArr[0].replaceAll("A", "U") + " ---> " + dnaSplitArr[0]);
-console.log(dnaSplitArr[1].replaceAll("T", "A") + " ---> " + dnaSplitArr[1]);
-console.log(dnaSplitArr[2].replaceAll("G", "C") + " ---> " + dnaSplitArr[2]);
-console.log(dnaSplitArr[3].replaceAll("C", "G") + " ---> " + dnaSplitArr[3]);
+// console.log(dnaSplitArr[0].replaceAll("A", "U") + " ---> " + dnaSplitArr[0]);
+// console.log(dnaSplitArr[1].replaceAll("T", "A") + " ---> " + dnaSplitArr[1]);
+// console.log(dnaSplitArr[2].replaceAll("G", "C") + " ---> " + dnaSplitArr[2]);
+// console.log(dnaSplitArr[3].replaceAll("C", "G") + " ---> " + dnaSplitArr[3]);
 //now I can replace all the dynamic parts with variables and then put it inside of a loop
 
 //incoming should be dnaSplitArr[i]
@@ -97,10 +104,8 @@ console.log(dnaSplitArr[3].replaceAll("C", "G") + " ---> " + dnaSplitArr[3]);
 //   console.log(dnaSplitArr.length)
 for (let i = 0; i < dnaSplitArr.length; i++) {
   //   console.log(dnaSplitArr[i]); // represents each letter of the string array //dnaSplitArr[i] typeof string, is a string value
-    console.log(dnaSplitArr[i]);
-    // console.log(dnaSplitArr.length)
-  
-
+  // console.log(dnaSplitArr[i]);
+  // console.log(dnaSplitArr.length)
   // let i = 0;
   // while (i < dnaSplitArr.length - 1) {
   // if (dnaSplitArr[i] === "A") {
@@ -112,19 +117,17 @@ for (let i = 0; i < dnaSplitArr.length; i++) {
   // } else if (dnaSplitArr[i] === "C") {
   //   dna.replaceAll("C", "G");
   // }
-
   //   console.log(dnaSplitArr);
   //   console.log(dna)
 }
 
 for (let j = 0; j < mRnaSplitArr.length; j++) {
   // while (j < mRnaSplitArr.length) {
-    console.log(mRnaSplitArr[j])
-  // }    
+  // console.log(mRnaSplitArr[j]);
+  // }
 }
 
-console.log(mRnaSplitArr.join(""))
-
+// console.log(mRnaSplitArr.join(""));
 
 // console.log(dnaSplitArr.join(""));
 // console.log(dna);
