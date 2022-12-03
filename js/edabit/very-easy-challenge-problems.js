f7f32b00a373a9a8a6843a7bd0b65312d5ff697c
diabm1 <diabm1855@gmail.com>
2022-12-03 10:14:01 -0500

    starting very easy challenge problems edabit

---
+++ js/edabit/very-easy-challenge-problems.js
@@ -0,0 +1,33 @@
+// intro challenge
+function hello() {
+  return "hello edabit.com";
+}
+
+//Buggy Code (Part 1)
+function cubes(a) {
+  return a ** 3;
+}
+
+//Convert Age to Days
+
+// Create a function that takes the age in years and returns the age in days.
+
+// Examples
+
+// calcAge(65) ➞ 23725
+
+// calcAge(0) ➞ 0
+
+// calcAge(20) ➞ 7300
+
+// Notes
+
+//     Use 365 days as the length of a year for this challenge.
+//     Ignore leap years and days between last birthday and now.
+//     Expect only positive integer inputs.
+
+function calcAge(age) {
+  // takes the age in years and returns the age in days.
+  // 1 year = 365 days
+  // 65 years = 
+}
